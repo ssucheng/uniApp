d9789239f777e9a875fe4d59e65505d3d40c218b
sucheng <943704372@qq.com>
2020-07-22 19:51:55 +0800

    上传图片和预览图片

---
+++ pages/photo/picture.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="">
+		<button type="primary" @click="choose">上传图片</button>
+		<view class="">
+				<image v-for="(item,index) in data" :key=index :src="item" mode="" @click="click1(item)"></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				data:[]
+			}
+		},
+		methods:{
+			choose(){
+				uni.chooseImage({
+					count:2,
+					success:res => {
+						this.data = res.tempFilePaths  //将文件的路径存在 data中循环遍历
+					}
+				})
+			},
+			click1(count){
+				uni.previewImage({
+					count,
+					urls:this.data
+				})
+			}
+		}
+	}
+	
+</script>
+
+<style>
+	
+</style>
