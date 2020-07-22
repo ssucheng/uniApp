3adf7fd33e639380857de4c591753a7ab0a6ec1b
sucheng <943704372@qq.com>
2020-07-22 21:54:49 +0800

    组件跳转方式

---
+++ pages/navigation/navgatot.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="">
+		<navigator url="/pages/index/index" open-type="switchTab">
+			<button type="default" @click="btn">跳转到index页面</button>
+		</navigator>
+	</view>
+</template>
+
+<script>
+	export default{
+		
+			data(){
+				return{
+					
+				}
+			},
+			methods:{
+				btn(){
+					console.log(1);
+				}
+			}
+		
+	}
+</script>
+
+<style>
+</style>
