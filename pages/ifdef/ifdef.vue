01fdf470f5512f5a1701acc555c62717cd9bacbc
sucheng <943704372@qq.com>
2020-07-22 20:36:07 +0800

    组件 api 样式条件注释的使用

---
+++ pages/ifdef/ifdef.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class="">
+		<!-- #ifdef H5 -->
+		<view class="box1">
+			h5页面会显示
+		</view>
+		<!-- #endif -->
+		<!-- #ifdef  MP-WEIXIN -->
+		<view class="box2">
+			微信页面会显示
+		</view>
+		<!-- #endif -->
+	</view>
+</template>
+
+<script>
+	export default{
+		onLoad(){
+			// #ifdef H5
+				console.log('h5');
+			// #endif
+			// #ifdef MP-WEIXIN
+				console.log('微信')
+			// #endif
+		}
+	}
+</script>
+
+<style>
+	/* #ifdef H5 */
+		view{
+			color: pink;
+		}
+	/* #endif */
+	/* #ifdef MP-WEIXIN */
+		view{
+			color: red;
+		}
+	/* #endif */
+</style>
